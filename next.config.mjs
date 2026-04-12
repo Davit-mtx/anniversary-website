939d5834b85220ce7ebd22e68756902305c15d80
Davit-mtx <firmansahdavit2@gmail.com>
2026-04-13 02:02:12 +0700

    update final

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
   output: "export",
   // PENTING: Ganti tulisan 'NAMA_REPO_ANDA' dengan nama persis repositori ini.
   // Misalnya jika nama reponya adalah 'anniversary-website', maka tulis: '/anniversary-website'
-  basePath: isProd ? "/anniversary" : "",
+  basePath: isProd ? "/anniversary-website" : "",
   images: {
     unoptimized: true,
   },
