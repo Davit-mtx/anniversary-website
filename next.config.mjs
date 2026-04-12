11c19df981c6882c4451b23752e51c38adc28d6b
Davit-mtx <firmansahdavit2@gmail.com>
2026-04-12 23:18:56 +0700

    update jekyll

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  // PENTING: Ganti tulisan 'NAMA_REPO_ANDA' dengan nama persis repositori ini.
+  // Misalnya jika nama reponya adalah 'anniversary-website', maka tulis: '/anniversary-website'
+  basePath: isProd ? "/anniversary" : "",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
